3b52eaab30193a12686b76156ec4ccb552b2e1dc
FullStack Dev <beauty@FullStacks-Mac-mini.local>
2019-04-11 23:56:34 +0800

    generate migration file

---
+++ database/migrations/2019_04_11_154323_create_teams_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('teams', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->string('name')->unique();
+			$table->tinyInteger('is_active')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('teams');
+	}
+}
