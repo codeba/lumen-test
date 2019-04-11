1a4e1b8631b096d8c948d10d177521934cdf2d96
FullStack Dev <beauty@FullStacks-Mac-mini.local>
2019-04-12 00:35:42 +0800

    add CRUD route in web.php

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,18 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+	return $router->app->version();
 });
+
+$router->get('users', 'UserController@index');
+$router->post('users', 'UserController@store');
+$router->put('users/{id}', 'UserController@update');
+$router->delete('users/{id}', 'UserController@destroy');
+
+$router->get('teams', 'TeamController@index');
+$router->post('teams', 'TeamController@store');
+$router->put('teams/{id}', 'TeamController@update');
+$router->delete('teams/{id}', 'TeamController@destroy');
